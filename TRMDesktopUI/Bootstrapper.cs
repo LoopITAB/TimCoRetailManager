055f9c5036446509d91c9e1db1bc846e10acc9a0
Lennie Fredriksson <Lennie.Fredriksson@outlook.com>
2020-04-14 06:11:02 +0200

    Add DI to TRMDesktopUI
    Starting to use Simple Container in Caliburn.Micro

--- TRMDesktopUI/Bootstrapper.cs
+++ TRMDesktopUI/Bootstrapper.cs
@@ -1,20 +1,59 @@
 ﻿using Caliburn.Micro;
+using System;
 using System.Windows;
+using System.Collections.Generic;
 using TRMDesktopUI.ViewModels;
+using System.Linq;
 
 namespace TRMDesktopUI
 {
     public class Bootstrapper : BootstrapperBase
     {
+        private SimpleContainer _container = new SimpleContainer();
+
         public Bootstrapper()
         {
             Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+            // we don't create interfaces for each ViewModel for now!
+            // that's why we have viewModelType in both places above!
+
         }
 
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             //base.OnStartup(sender, e);
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }
