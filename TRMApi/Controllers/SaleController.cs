4f346fe2267e5a71702e790602a06354b926af86
Lennie Fredriksson <Lennie.Fredriksson@outlook.com>
2020-05-05 21:44:10 +0200

    Upgrading to .NET CORE for Back-End API Project

---
+++ TRMApi/Controllers/SaleController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class SaleController : ControllerBase
+    {
+        [Authorize(Roles = "Cashier")]
+        public void Post([FromBody] SaleModel sale)
+        {
+            SaleData data = new SaleData();
+
+            //string userId = RequestContext.Principal.Identity.GetUserId();
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            data.SaveSale(sale, userId);
+        }
+
+        [Authorize(Roles = "Admin,Manager")]
+        [Route("GetSalesReport")]
+        public List<SaleReportModel> GetSalesReport()
+        {
+            SaleData data = new SaleData();
+
+            return data.GetSaleReport();
+        }
+    }
+}
---
+++ TRMApi/Controllers/UserController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using TRMApi.Data;
+using TRMApi.Models;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public UserModel GetById()
+        {
+            //string userId = RequestContext.Principal.Identity.GetUserId();
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            UserData data = new UserData();
+
+            return data.GetUserById(userId).First(); ;
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        [Route("Admin/GetAllUsers")]
+        public List<ApplicationUserModel> GetAllUsers()
+        {
+            //var userStore = new UserStore<ApplicationUser>(context);
+
+            List<ApplicationUserModel> output = new List<ApplicationUserModel>();
+
+            //var userStore = new UserStore<ApplicationUser>(_context);
+            //var userManager = new UserManager<ApplicationUser>(userStore);
+
+            //var users = userManager.Users.ToList();
+            var users = _context.Users.ToList();
+            //var roles = _context.Roles.ToList();
+
+            var userRoles = from ur in _context.UserRoles
+                            join r in _context.Roles on ur.RoleId equals r.Id
+                            select new { ur.UserId, ur.RoleId, r.Name };
+
+            foreach (var user in users)
+            {
+                ApplicationUserModel u = new ApplicationUserModel
+                {
+                    Id = user.Id,
+                    Email = user.Email
+                };
+
+                u.Roles = userRoles.Where(x => x.UserId == u.Id).ToDictionary(key => key.RoleId, val => val.Name);
+
+                //foreach (var role in user.Roles)
+                //{
+                //    u.Roles.Add(role.RoleId, roles.Where(x => x.Id == role.RoleId).First().Name);
+                //}
+
+                output.Add(u);
+            }
+
+            return output;
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        [Route("Admin/GetAllRoles")]
+        public Dictionary<string, string> GetAllRoles()
+        {
+            var roles = _context.Roles.ToDictionary(x => x.Id, x => x.Name);
+
+            return roles;
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [Route("Admin/AddRole")]
+        public async Task AddARole([FromBody] UserRolePairModel pairing)
+        {
+            //var userStore = new UserStore<ApplicationUser>(context);
+            //var userManager = new UserManager<ApplicationUser>(userStore);
+
+            var user = await _userManager.FindByIdAsync(pairing.UserId);
+            await _userManager.AddToRoleAsync(user, pairing.RoleName);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [Route("Admin/RemoveRole")]
+        public async Task RemoveARole([FromBody] UserRolePairModel pairing)
+        {
+            //var userStore = new UserStore<ApplicationUser>(context);
+            //var userManager = new UserManager<ApplicationUser>(userStore);
+
+            var user = await _userManager.FindByIdAsync(pairing.UserId);
+            await _userManager.RemoveFromRoleAsync(user, pairing.RoleName);
+        }
+    }
+}
